6e383c9c0d2a18143d82a12bbd246bb07812fb6b
Thuc Pham <thuc.pham@terralogic.com>
2024-07-10 13:30:29 +0700

    Initial create

---
+++ NET8.Demo.Service1/Controllers/BaseApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace NET8.Demo.Service1.Controllers;
+
+public class BaseApiController : ControllerBase
+{
+    protected readonly IHttpContextAccessor _httpContextAccessor;
+
+    public string UserId { get; set; }
+    public string Email { get; set; }
+    public string Roles { get; set; }
+    public string AccessToken { get; set; }
+
+    public BaseApiController(IHttpContextAccessor httpContextAccessor)
+    {
+
+        _httpContextAccessor = httpContextAccessor;
+
+        var httpContext = _httpContextAccessor.HttpContext;
+
+        var identity = httpContext?.User.Identity as ClaimsIdentity;
+
+        UserId = identity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        Email = identity?.FindFirst(ClaimTypes.Email)?.Value;
+        Roles = identity?.FindFirst(ClaimTypes.Role)?.Value;
+
+        AccessToken = httpContext?.GetTokenAsync("access_token").Result;
+    }
+}
---
+++ NET8.Demo.Service1/Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["Identity:Authority"];
+    options.Audience = "service1_api";
+    options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+});
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
